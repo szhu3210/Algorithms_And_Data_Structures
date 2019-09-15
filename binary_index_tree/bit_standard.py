796bcc45f1e83e019b29481cb0ea897bb848dac5
Shengxiang Zhu (Troy) <szhu@email.arizona.edu>
2019-09-15 09:52:26 -0700

    Added standard implementation of Binary Indexed Tree (BIT).

---
+++ binary_index_tree/bit_standard.py
@@ -0,0 +1,75 @@
+"""
+BIT implementation
+"""
+
+
+class BIT:
+    """
+    BIT data structure is used to calculate prefix sum of an array in O(logN) time,
+     and can update value in O(logN) time.
+    Space complexity is O(N).
+    """
+
+    def __init__(self, p):
+        if isinstance(p, int):
+            self.size = p
+            self.bit = [0] * (self.size + 1)
+        elif isinstance(p, list):
+            self.size = len(p)
+            self.bit = [0] * (self.size + 1)
+            for i, val in enumerate(p):
+                self.set(i, val)
+
+    def check_index(self, i):
+        """
+        check if index i is within the range of the BIT data structure.
+        :param i: int
+        :return: None
+        """
+        if not 0 <= i < self.size:
+            raise ValueError(f'Index ({i}) is out of range '
+                             f'([{0}, {self.size - 1}]) of the BIT array!')
+
+    def get(self, i):
+        """
+        Get prefix sum of index i, inclusive.
+        :param i: int
+        :return: int
+        """
+        self.check_index(i)
+        prefix_sum = 0
+        i += 1
+        while i > 0:
+            prefix_sum += self.bit[i]
+            i -= i & (-i)
+        return prefix_sum
+
+    def set(self, i, val):
+        """
+        Update value of index i.
+        :param i: int
+        :param val: int, incremental
+        :return: None
+        """
+        self.check_index(i)
+        i += 1
+        while i <= self.size:
+            self.bit[i] += val
+            i += i & (-i)
+
+
+def test():
+    """
+    Test BIT class.
+    :return: None
+    """
+    freq = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    bit = BIT(freq)
+    print(f"Sum of elements in arr[0..5] is {bit.get(5)}.")
+
+    bit.set(3, 6)
+    print(f"Sum of elements in arr[0..5] after update is {bit.get(5)}.")
+
+
+test()
